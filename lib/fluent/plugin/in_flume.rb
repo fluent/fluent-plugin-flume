17b18723879c69149fc5c87cce8354e5f31260de
Muga Nishizawa <muga.nishizawa@gmail.com>
2013-01-20 10:06:24 +0900

    changed copyright notations

--- lib/fluent/plugin/in_flume.rb
+++ lib/fluent/plugin/in_flume.rb
@@ -1,7 +1,7 @@
 #
 # Fluent
 #
-# Copyright (C) 2012 Muga Nishizawa
+# Copyright (C) 2012 - 2013 Muga Nishizawa
 #
 #    Licensed under the Apache License, Version 2.0 (the "License");
 #    you may not use this file except in compliance with the License.
