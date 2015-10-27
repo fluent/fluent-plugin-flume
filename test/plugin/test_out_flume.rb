e8b32c3000f926e3c542f14d01685f6534fde152
Hiroshi Hatake <hatake@clear-code.com>
2015-10-27 16:15:31 +0900

    Use stubbed write method in test

--- test/plugin/test_out_flume.rb
+++ test/plugin/test_out_flume.rb
@@ -3,13 +3,21 @@ require 'fluent/test'
 require 'lib/fluent/plugin/out_flume'
 
 class FlumeOutputTest < Test::Unit::TestCase
+  def setup
+    Fluent::Test.setup
+  end
+
   CONFIG = %[
     host 127.0.0.1
     port 35862
   ]
 
   def create_driver(conf=CONFIG, tag='test')
-    Fluent::Test::BufferedOutputTestDriver.new(Fluent::FlumeOutput, tag).configure(conf)
+    Fluent::Test::BufferedOutputTestDriver.new(Fluent::FlumeOutput, tag)do
+      def write(chunk)
+        chunk.read
+      end
+    end.configure(conf)
   end
 
   def test_configure
@@ -82,24 +90,26 @@ class FlumeOutputTest < Test::Unit::TestCase
     d.emit({"k11"=>"v11", "k12"=>"v12"}, time)
     d.emit({"k21"=>"v21", "k22"=>"v22"}, time)
     d.run
+    emits = d.emits
     assert_equal [
         [d.tag, time, {"k11"=>"v11", "k12"=>"v12"}.to_json.to_s],
         [d.tag, time, {"k21"=>"v21", "k22"=>"v22"}.to_json.to_s],
-      ], $handler.last
-    $handler.last_clear
+      ], emits
 
     d = create_driver(CONFIG + %[
       remove_prefix test
     ], 'test.flumeplugin')
     assert_equal 'test.flumeplugin', d.tag
-    d.emit({"k11"=>"v11", "k12"=>"v12"}, time)
-    d.emit({"k21"=>"v21", "k22"=>"v22"}, time)
-    d.run
+    d.run do
+      d.emit({"k11"=>"v11", "k12"=>"v12"}, time)
+      d.emit({"k21"=>"v21", "k22"=>"v22"}, time)
+    end
+
+    emits = d.emits
     assert_equal [
         ['flumeplugin', time, {"k11"=>"v11", "k12"=>"v12"}.to_json.to_s],
         ['flumeplugin', time, {"k21"=>"v21", "k22"=>"v22"}.to_json.to_s],
-      ], $handler.last
-    $handler.last_clear
+      ], emits
 
     d = create_driver(CONFIG + %[
       remove_prefix test
@@ -107,10 +117,10 @@ class FlumeOutputTest < Test::Unit::TestCase
     assert_equal 'xxx.test.flumeplugin', d.tag
     d.emit({"k11"=>"v11", "k12"=>"v12"}, time)
     d.run
+    emits = d.emits
     assert_equal [
         ['xxx.test.flumeplugin', time, {"k11"=>"v11", "k12"=>"v12"}.to_json.to_s],
-      ], $handler.last
-    $handler.last_clear
+      ], emits
 
     d = create_driver(CONFIG + %[
       remove_prefix test
@@ -118,60 +128,9 @@ class FlumeOutputTest < Test::Unit::TestCase
     assert_equal 'test', d.tag
     d.emit({"k11"=>"v11", "k12"=>"v12"}, time)
     d.run
+    emits = d.emits
     assert_equal [
         [d.instance.default_category, time, {"k11"=>"v11", "k12"=>"v12"}.to_json.to_s],
-      ], $handler.last
-    $handler.last_clear
+      ], emits
   end
-
-  def setup
-    Fluent::Test.setup
-    Fluent::FlumeOutput.new
-    $handler = TestFlumeServerHandler.new
-    @dummy_server_thread = Thread.new do
-      begin
-        transport = Thrift::ServerSocket.new '127.0.0.1', 35862
-        processor = ThriftFlumeEventServer::Processor.new $handler
-        transport_factory = Thrift::BufferedTransportFactory.new
-        protocol_factory = Thrift::BinaryProtocolFactory.new
-        protocol_factory.instance_eval {|obj|
-          def get_protocol(trans) # override
-            Thrift::BinaryProtocol.new(trans, strict_read=false, strict_write=false)
-          end
-        }
-        server = Thrift::SimpleServer.new processor, transport, transport_factory, protocol_factory
-        server.serve
-      ensure
-        #transport.close unless transport.closed?
-	transport.close if transport
-      end
-    end
-    sleep 0.1 # boo...
-  end
-
-  def teardown
-    @dummy_server_thread.kill
-    @dummy_server_thread.join
-  end
-
-  class TestFlumeServerHandler
-    attr :last
-    def initialize
-      @last = []
-    end
-    def append(evt)
-      @last << [evt.headers, evt.body]
-    end
-    def rawAppend(evt)
-    end
-    def ackedAppend(evt)
-      EventStatus::OK
-    end
-    def close
-    end
-    def last_clear
-      @last.clear
-    end
-  end
-
 end
