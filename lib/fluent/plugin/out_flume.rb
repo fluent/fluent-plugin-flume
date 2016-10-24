414e77112ecc882625c11ee87c4af45aca16e755
Masahiro Nakagawa <repeatedly@gmail.com>
2016-10-25 03:24:51 +0900

    Add explicit require

--- lib/fluent/plugin/out_flume.rb
+++ lib/fluent/plugin/out_flume.rb
@@ -15,6 +15,9 @@
 #    See the License for the specific language governing permissions and
 #    limitations under the License.
 #
+
+require 'fluent/output'
+
 module Fluent
 
 class FlumeOutput < BufferedOutput
