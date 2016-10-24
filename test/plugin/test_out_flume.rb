13e253c49d4a16d153269415f1638e2e22ce76a5
Masahiro Nakagawa <repeatedly@gmail.com>
2016-10-25 03:25:26 +0900

    Fix out_flume test

--- test/plugin/test_out_flume.rb
+++ test/plugin/test_out_flume.rb
@@ -13,7 +13,7 @@ class FlumeOutputTest < Test::Unit::TestCase
   ]
 
   def create_driver(conf=CONFIG, tag='test')
-    Fluent::Test::BufferedOutputTestDriver.new(Fluent::FlumeOutput, tag)do
+    Fluent::Test::BufferedOutputTestDriver.new(Fluent::FlumeOutput, tag) do
       def write(chunk)
         chunk.read
       end
@@ -41,8 +41,9 @@ class FlumeOutputTest < Test::Unit::TestCase
     d = create_driver
     d.emit({"k11"=>"v11", "k12"=>"v12"}, time)
     d.emit({"k21"=>"v21", "k22"=>"v22"}, time)
-    d.expect_format [d.tag, time, {"k11"=>"v11", "k12"=>"v12"}].to_msgpack
-    d.expect_format [d.tag, time, {"k21"=>"v21", "k22"=>"v22"}].to_msgpack
+    puts ({"k11" => "v11", "k12" => "v12"}.to_json)
+    d.expect_format [d.tag, time, {"k11"=>"v11", "k12"=>"v12"}.to_json].to_msgpack
+    d.expect_format [d.tag, time, {"k21"=>"v21", "k22"=>"v22"}.to_json].to_msgpack
     d.run
 
     d = create_driver(CONFIG + %[
@@ -52,8 +53,8 @@ class FlumeOutputTest < Test::Unit::TestCase
 
     d.emit({"k11"=>"v11", "k12"=>"v12"}, time)
     d.emit({"k21"=>"v21", "k22"=>"v22"}, time)
-    d.expect_format ['flumeplugin', time, {"k11"=>"v11", "k12"=>"v12"}].to_msgpack
-    d.expect_format ['flumeplugin', time, {"k21"=>"v21", "k22"=>"v22"}].to_msgpack
+    d.expect_format ['flumeplugin', time, {"k11"=>"v11", "k12"=>"v12"}.to_json].to_msgpack
+    d.expect_format ['flumeplugin', time, {"k21"=>"v21", "k22"=>"v22"}.to_json].to_msgpack
     d.run
 
     d = create_driver(CONFIG + %[
@@ -61,7 +62,7 @@ class FlumeOutputTest < Test::Unit::TestCase
     ], 'xxx.test.flumeplugin')
     assert_equal 'xxx.test.flumeplugin', d.tag
     d.emit({"k11"=>"v11", "k12"=>"v12"}, time)
-    d.expect_format ['xxx.test.flumeplugin', time, {"k11"=>"v11", "k12"=>"v12"}].to_msgpack
+    d.expect_format ['xxx.test.flumeplugin', time, {"k11"=>"v11", "k12"=>"v12"}.to_json].to_msgpack
     d.run
 
     d = create_driver(CONFIG + %[
@@ -69,7 +70,7 @@ class FlumeOutputTest < Test::Unit::TestCase
     ], 'test')
     assert_equal 'test', d.tag
     d.emit({"k11"=>"v11", "k12"=>"v12"}, time)
-    d.expect_format ['unknown', time, {"k11"=>"v11", "k12"=>"v12"}].to_msgpack
+    d.expect_format ['unknown', time, {"k11"=>"v11", "k12"=>"v12"}.to_json].to_msgpack
     d.run
 
     d = create_driver(CONFIG + %[
@@ -78,8 +79,8 @@ class FlumeOutputTest < Test::Unit::TestCase
     assert_equal 'test.flumeplugin', d.tag
     d.emit({"k11"=>"v11", "k12"=>"v12"}, time)
     d.emit({"k21"=>"v21", "k22"=>"v22"}, time)
-    d.expect_format ['flumeplugin', time, {"k11"=>"v11", "k12"=>"v12"}].to_msgpack
-    d.expect_format ['flumeplugin', time, {"k21"=>"v21", "k22"=>"v22"}].to_msgpack
+    d.expect_format ['flumeplugin', time, {"k11"=>"v11", "k12"=>"v12"}.to_json].to_msgpack
+    d.expect_format ['flumeplugin', time, {"k21"=>"v21", "k22"=>"v22"}.to_json].to_msgpack
     d.run
   end
 
