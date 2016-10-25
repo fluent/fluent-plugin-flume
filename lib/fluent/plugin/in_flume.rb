d1b222fec61001b4de983b4177d2f995e8d8acdb
Masahiro Nakagawa <repeatedly@gmail.com>
2016-10-25 20:01:30 +0900

    Fix weird indent

--- lib/fluent/plugin/in_flume.rb
+++ lib/fluent/plugin/in_flume.rb
@@ -19,175 +19,169 @@
 require 'fluent/input'
 
 module Fluent
+  class FlumeInput < Input
+    Plugin.register_input('flume', self)
 
-class FlumeInput < Input
-  Plugin.register_input('flume', self)
+    config_param :port,            :integer, :default => 56789
+    config_param :bind,            :string,  :default => '0.0.0.0'
+    config_param :server_type,     :string,  :default => 'simple'
+    config_param :is_framed,       :bool,    :default => false
+    config_param :body_size_limit, :size,    :default => 32 * 1024 * 1024
+    config_param :tag_field,	 :string,  :default => nil
+    config_param :default_tag,	 :string,  :default => 'category'
+    config_param :add_prefix,      :string,  :default => nil
+    config_param :msg_format,      :string,  :default => 'text'
 
-  config_param :port,            :integer, :default => 56789
-  config_param :bind,            :string,  :default => '0.0.0.0'
-  config_param :server_type,     :string,  :default => 'simple'
-  config_param :is_framed,       :bool,    :default => false
-  config_param :body_size_limit, :size,    :default => 32 * 1024 * 1024
-  config_param :tag_field,	 :string,  :default => nil
-  config_param :default_tag,	 :string,  :default => 'category'
-  config_param :add_prefix,      :string,  :default => nil
-  config_param :msg_format,      :string,  :default => 'text'
-
-  unless method_defined?(:log)
-    define_method(:log) { $log }
-  end
-
-  def initialize
-    require 'thrift'
-    require 'fluent/plugin/old_thrift/flume_types'
-    $:.unshift File.join(File.dirname(__FILE__), 'old_thrift')
-    require 'flume_constants'
-    require 'thrift_flume_event_server'
-    super
-  end
-
-  def configure(conf)
-    super
-  end
-
-  def start
-    log.debug "listening flume on #{@bind}:#{@port}"
-    log.warn "[in_flume] This plugin is obsoleted for Flume NG. You should consider to use flume-ng-fluentd-sink."
-
-    handler = FluentFlumeHandler.new
-    handler.tag_field = @tag_field
-    handler.default_tag = @default_tag
-    handler.add_prefix = @add_prefix
-    handler.msg_format = @msg_format
-    handler.log = log
-    processor = ThriftFlumeEventServer::Processor.new handler
-
-    @transport = Thrift::ServerSocket.new @bind, @port
-    unless @is_framed
-      transport_factory = Thrift::BufferedTransportFactory.new
-    else
-      raise ConfigError, "in_flume: unsupported is_framed '#{@is_framed}'"
+    unless method_defined?(:log)
+      define_method(:log) { $log }
     end
 
-    unless ['text', 'json'].include? @msg_format
-      raise 'Unknown format: msg_format=#{@msg_format}'
+    def initialize
+      require 'thrift'
+      require 'fluent/plugin/old_thrift/flume_types'
+      $:.unshift File.join(File.dirname(__FILE__), 'old_thrift')
+      require 'flume_constants'
+      require 'thrift_flume_event_server'
+      super
     end
 
-    # 2011/09/29 Kazuki Ohta <kazuki.ohta@gmail.com>
-    # This section is a workaround to set strict_read and strict_write option.
-    # Ruby-Thrift 0.7 set them both 'true' in default, but Flume protocol set
-    # them both 'false'.
-    protocol_factory = Thrift::BinaryProtocolFactory.new
-    protocol_factory.instance_eval {|obj|
-      def get_protocol(trans) # override
-        return Thrift::BinaryProtocol.new(trans,
-                                          strict_read=false,
-                                          strict_write=false)
-      end
-    }
+    def start
+      log.debug "listening flume on #{@bind}:#{@port}"
+      log.warn "[in_flume] This plugin is obsoleted for Flume NG. You should consider to use flume-ng-fluentd-sink."
 
-    case @server_type
-    when 'simple'
-      @server = Thrift::SimpleServer.new processor, @transport, transport_factory, protocol_factory
-    when 'threaded'
-      @server = Thrift::ThreadedServer.new processor, @transport, transport_factory, protocol_factory
-    when 'thread_pool'
-      @server = Thrift::ThreadPoolServer.new processor, @transport, transport_factory, protocol_factory
-    else
-      raise ConfigError, "in_flume: unsupported server_type '#{@server_type}'"
-    end
-    @thread = Thread.new(&method(:run))
-  end
+      handler = FluentFlumeHandler.new
+      handler.tag_field = @tag_field
+      handler.default_tag = @default_tag
+      handler.add_prefix = @add_prefix
+      handler.msg_format = @msg_format
+      handler.log = log
+      processor = ThriftFlumeEventServer::Processor.new handler
 
-  def shutdown
-    @transport.close unless @transport.closed?
-    #@thread.join
-  end
-
-  def run
-    log.debug "starting server: #{@server}"
-    @server.serve
-  rescue
-    log.error "unexpected error", :error=>$!.to_s
-    log.error_backtrace
-  end
-
-  class FluentFlumeHandler
-    attr_accessor :tag_field
-    attr_accessor :default_tag
-    attr_accessor :add_prefix
-    attr_accessor :msg_format
-    attr_accessor :log
-
-    def append(evt)
-      begin
-        record = create_record(evt)
-        if @tag_field
-          tag = evt.fieldss[@tag_field] || @default_tag
-          unless tag
-            return # ignore
-          end
-        else
-          tag = @default_tag
-        end
-        timestamp = evt.timestamp.to_i
-        if @add_prefix
-          Engine.emit(@add_prefix + '.' + tag, timestamp, record)
-        else
-          Engine.emit(tag, timestamp, record)
-        end
-      rescue => e
-        log.error "unexpected error", :error=>$!.to_s
-        log.error_backtrace
-      end
-    end
-
-    def rawAppend(evt)
-      log.error "rawAppend is not implemented yet: #{evt}"
-    end
-
-    def ackedAppend(evt)
-      begin
-        record = create_record(evt)
-        if @tag_field
-          tag = evt.fieldss[@tag_field] || @default_tag
-          unless tag
-            return # ignore
-          end
-        else
-          tag = @default_tag
-        end
-        timestamp = evt.timestamp.to_i
-        if @add_prefix
-          Engine.emit(@add_prefix + '.' + tag, timestamp, record)
-        else
-          Engine.emit(tag, timestamp, record)
-        end
-        return EventStatus::ACK
-      rescue => e
-        log.error "unexpected error", :error=>$!.to_s
-        log.error_backtrace
-        return EventStatus::ERR
-      end
-    end
-
-    def close()
-    end
-
-    private
-    def create_record(evt)
-      case @msg_format
-      when 'text'
-        return {'message'=>evt.body.force_encoding('UTF-8')}
-      when 'json'
-        js = JSON.parse(evt.body.force_encoding('UTF-8'))
-        raise 'body must be a Hash, if json_body=true' unless js.is_a?(Hash)
-        return js
+      @transport = Thrift::ServerSocket.new @bind, @port
+      unless @is_framed
+        transport_factory = Thrift::BufferedTransportFactory.new
       else
-        raise 'Invalid format: #{@msg_format}'
+        raise ConfigError, "in_flume: unsupported is_framed '#{@is_framed}'"
+      end
+
+      unless ['text', 'json'].include? @msg_format
+        raise 'Unknown format: msg_format=#{@msg_format}'
+      end
+
+      # 2011/09/29 Kazuki Ohta <kazuki.ohta@gmail.com>
+      # This section is a workaround to set strict_read and strict_write option.
+      # Ruby-Thrift 0.7 set them both 'true' in default, but Flume protocol set
+      # them both 'false'.
+      protocol_factory = Thrift::BinaryProtocolFactory.new
+      protocol_factory.instance_eval {|obj|
+        def get_protocol(trans) # override
+          return Thrift::BinaryProtocol.new(trans,
+                                            strict_read=false,
+                                            strict_write=false)
+        end
+      }
+
+      case @server_type
+      when 'simple'
+        @server = Thrift::SimpleServer.new processor, @transport, transport_factory, protocol_factory
+      when 'threaded'
+        @server = Thrift::ThreadedServer.new processor, @transport, transport_factory, protocol_factory
+      when 'thread_pool'
+        @server = Thrift::ThreadPoolServer.new processor, @transport, transport_factory, protocol_factory
+      else
+        raise ConfigError, "in_flume: unsupported server_type '#{@server_type}'"
+      end
+      @thread = Thread.new(&method(:run))
+    end
+
+    def shutdown
+      @transport.close unless @transport.closed?
+      #@thread.join
+    end
+
+    def run
+      log.debug "starting server: #{@server}"
+      @server.serve
+    rescue
+      log.error "unexpected error", :error=>$!.to_s
+      log.error_backtrace
+    end
+
+    class FluentFlumeHandler
+      attr_accessor :tag_field
+      attr_accessor :default_tag
+      attr_accessor :add_prefix
+      attr_accessor :msg_format
+      attr_accessor :log
+
+      def append(evt)
+        begin
+          record = create_record(evt)
+          if @tag_field
+            tag = evt.fieldss[@tag_field] || @default_tag
+            unless tag
+              return # ignore
+            end
+          else
+            tag = @default_tag
+          end
+          timestamp = evt.timestamp.to_i
+          if @add_prefix
+            Engine.emit(@add_prefix + '.' + tag, timestamp, record)
+          else
+            Engine.emit(tag, timestamp, record)
+          end
+        rescue => e
+          log.error "unexpected error", :error=>$!.to_s
+          log.error_backtrace
+        end
+      end
+
+      def rawAppend(evt)
+        log.error "rawAppend is not implemented yet: #{evt}"
+      end
+
+      def ackedAppend(evt)
+        begin
+          record = create_record(evt)
+          if @tag_field
+            tag = evt.fieldss[@tag_field] || @default_tag
+            unless tag
+              return # ignore
+            end
+          else
+            tag = @default_tag
+          end
+          timestamp = evt.timestamp.to_i
+          if @add_prefix
+            Engine.emit(@add_prefix + '.' + tag, timestamp, record)
+          else
+            Engine.emit(tag, timestamp, record)
+          end
+          return EventStatus::ACK
+        rescue => e
+          log.error "unexpected error", :error=>$!.to_s
+          log.error_backtrace
+          return EventStatus::ERR
+        end
+      end
+
+      def close()
+      end
+
+      private
+      def create_record(evt)
+        case @msg_format
+        when 'text'
+          return {'message'=>evt.body.force_encoding('UTF-8')}
+        when 'json'
+          js = JSON.parse(evt.body.force_encoding('UTF-8'))
+          raise 'body must be a Hash, if json_body=true' unless js.is_a?(Hash)
+          return js
+        else
+          raise 'Invalid format: #{@msg_format}'
+        end
       end
     end
   end
-end
-
 end
--- lib/fluent/plugin/out_flume.rb
+++ lib/fluent/plugin/out_flume.rb
@@ -19,91 +19,85 @@
 require 'fluent/output'
 
 module Fluent
+  class FlumeOutput < BufferedOutput
+    Fluent::Plugin.register_output('flume', self)
 
-class FlumeOutput < BufferedOutput
-  Fluent::Plugin.register_output('flume', self)
+    config_param :host,      :string,  :default => 'localhost'
+    config_param :port,      :integer, :default => 35863
+    config_param :timeout,   :integer, :default => 30
+    config_param :remove_prefix,    :string, :default => nil
+    config_param :default_category, :string, :default => 'unknown'
+    desc "The format of the thrift body. (default: json)"
+    config_param :format, :string, default: 'json'
+    config_param :trim_nl, :bool, default: true
 
-  config_param :host,      :string,  :default => 'localhost'
-  config_param :port,      :integer, :default => 35863
-  config_param :timeout,   :integer, :default => 30
-  config_param :remove_prefix,    :string, :default => nil
-  config_param :default_category, :string, :default => 'unknown'
-  desc "The format of the thrift body. (default: json)"
-  config_param :format, :string, default: 'json'
-  config_param :trim_nl, :bool, default: true
+    unless method_defined?(:log)
+      define_method(:log) { $log }
+    end
 
-  unless method_defined?(:log)
-    define_method(:log) { $log }
-  end
+    def initialize
+      require 'thrift'
+      require 'fluent/plugin/thrift/flume_types'
+      $:.unshift File.join(File.dirname(__FILE__), 'thrift')
+      require 'flume_constants'
+      require 'thrift_source_protocol'
+      super
+    end
 
-  def initialize
-    require 'thrift'
-    require 'fluent/plugin/thrift/flume_types'
-    $:.unshift File.join(File.dirname(__FILE__), 'thrift')
-    require 'flume_constants'
-    require 'thrift_source_protocol'
-    super
-  end
+    def configure(conf)
+      # override default buffer_chunk_limit
+      conf['buffer_chunk_limit'] ||= '1m'
 
-  def configure(conf)
-    # override default buffer_chunk_limit
-    conf['buffer_chunk_limit'] ||= '1m'
+      super
 
-    super
+      @formatter = Plugin.new_formatter(@format)
+      @formatter.configure(conf)
+    end
 
-    @formatter = Plugin.new_formatter(@format)
-    @formatter.configure(conf)
-  end
+    def start
+      super
 
-  def start
-    super
+      if @remove_prefix
+        @removed_prefix_string = @remove_prefix + '.'
+        @removed_length = @removed_prefix_string.length
+      end
+    end
 
-    if @remove_prefix
-      @removed_prefix_string = @remove_prefix + '.'
-      @removed_length = @removed_prefix_string.length
+    def format(tag, time, record)
+      if @remove_prefix and
+         ((tag[0, @removed_length] == @removed_prefix_string and tag.length > @removed_length) or tag == @remove_prefix)
+        tag = (tag[@removed_length..-1] || @default_category)
+      end
+      fr = @formatter.format(tag, time, record)
+      fr.chomp! if @trim_nl
+      [tag, time, fr].to_msgpack
+    end
+
+    def write(chunk)
+      socket = Thrift::Socket.new @host, @port, @timeout
+      transport = Thrift::FramedTransport.new socket
+      #protocol = Thrift::BinaryProtocol.new transport, false, false
+      protocol = Thrift::CompactProtocol.new transport
+      client = ThriftSourceProtocol::Client.new protocol
+
+      count = 0
+      header = {}
+      transport.open
+      log.debug "thrift client opened: #{client}"
+      begin
+        chunk.msgpack_each { |tag, time, record|
+          header['timestamp'.freeze] = time.to_s
+          header['tag'.freeze] = tag
+          entry = ThriftFlumeEvent.new(:body    => record.force_encoding('UTF-8'),
+                                       :headers => header)
+          client.append entry
+          count += 1
+        }
+        log.debug "Writing #{count} entries to flume"
+      ensure
+        log.debug "thrift client closing: #{client}"
+        transport.close
+      end
     end
   end
-
-  def shutdown
-    super
-  end
-
-  def format(tag, time, record)
-    if @remove_prefix and
-        ((tag[0, @removed_length] == @removed_prefix_string and tag.length > @removed_length) or tag == @remove_prefix)
-      tag = (tag[@removed_length..-1] || @default_category)
-    end
-    fr = @formatter.format(tag, time, record)
-    fr.chomp! if @trim_nl
-    [tag, time, fr].to_msgpack
-  end
-
-  def write(chunk)
-    socket = Thrift::Socket.new @host, @port, @timeout
-    transport = Thrift::FramedTransport.new socket
-    #protocol = Thrift::BinaryProtocol.new transport, false, false
-    protocol = Thrift::CompactProtocol.new transport
-    client = ThriftSourceProtocol::Client.new protocol
-
-    count = 0
-    header = {}
-    transport.open
-    log.debug "thrift client opened: #{client}"
-    begin
-      chunk.msgpack_each { |tag, time, record|
-        header['timestamp'.freeze] = time.to_s
-        header['tag'.freeze] = tag
-        entry = ThriftFlumeEvent.new(:body    => record.force_encoding('UTF-8'),
-                                     :headers => header)
-        client.append entry
-        count += 1
-      }
-      log.debug "Writing #{count} entries to flume"
-    ensure
-      log.debug "thrift client closing: #{client}"
-      transport.close
-    end
-  end
-end
-
 end
